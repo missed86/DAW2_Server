d0bd70d5b9b92f828e01eafdc1fefc3571f7bfa9
missed86 <jimmy11286@gmail.com>
2022-10-18 09:58:55 +0200

    next

---
+++ UT02/Tanda2/01.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UT02 - Tanda2 - Ejercicio 1</title>
+</head>
+<body>
+    <h1>Area de un rectangulo</h1>
+    <form action="" method="post">
+        Alto: <input type="number" name="a" value="<?php if (isset($_POST['a'])) echo $_POST['a'] ?>">
+        Ancho: <input type="number" name="b" value="<?php if (isset($_POST['b'])) echo $_POST['b'] ?>">
+        <input type="submit" name="enviar" value="Calcula">
+        <?php
+            if (isset($_POST['a']) && isset($_POST['a'])) {
+                $a = $_POST['a'];
+                $b = $_POST['b'];
+                echo "<br><br>";
+                echo $a*$b." m<sup>2</sup>";
+                
+            }
+        ?>
+    </form>
+</body>
+</html>
