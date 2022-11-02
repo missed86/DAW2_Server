35f8ff6989932fe990c844583292f3f8e65d9c14
missed86 <jimmy11286@gmail.com>
2022-11-02 09:04:26 +0100

    sdfdffed

--- PHP/UT02/Tanda3/01.php
+++ PHP/UT02/Tanda3/01.php
@@ -10,7 +10,7 @@
     <h2>Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle for.</h2>
     <ul>
     <?php 
-    for ($i=5; $i <= 100; $i+=5) { 
+    for ($i=0; $i <= 100; $i+=5) { 
         echo "<li>" . $i ."</li>";
     }
     ?>
